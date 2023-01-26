d0b99721411c985ea7b9c5479a14b95cce54a586
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-01-26 21:34:33 +0800

    style: 迁移不相关的core代码

--- clis/idcard.py
+++ clis/idcard.py
@@ -1,13 +1,85 @@
+import json
 import sys
+from typing import Any, Sequence, Iterable
+from datetime import date
 from itertools import product
 from math import ceil
-from typing import Any
 
-import click
-from click import Parameter, Context, ParamType
+from click import Parameter, Context, ParamType, command, option
 
-from core.idcard import enum_adcode, enum_seq, enum_birth_by_ymd, patch_checksum, enum_birth_by_age
 from core.meaningfuls import fmt_datasize
+from core.structs import MorseSegment, Segment
+
+RIGHTS = (7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2)
+
+with open(r'./code2022.json', 'r', encoding='UTF-8') as f:
+    ad_codes = tuple(k[:6] for k in json.load(f).keys() if k.endswith('000000'))
+
+
+def mk_date(year: int, month: int, day: int) -> str | None:
+    try:
+        return date(year, month, day).strftime('%Y%m%d')
+    except ValueError:
+        return None
+
+
+def enum_adcode(
+        provinces: Sequence = None,
+        cities: Sequence = None,
+        counties: Sequence = None,
+) -> Iterable:
+    codes = (c for c in ad_codes if not c.endswith('0000'))
+    codes = (c for c in codes if c[0:2] in provinces) if provinces else codes
+    codes = (c for c in codes if c[2:4] in cities) if cities else codes
+    codes = (c for c in codes if c[4:6] in counties) if counties else codes
+    return codes
+
+
+def enum_birth_by_ymd(
+        year: Iterable = None,
+        month: Iterable = None,
+        day: Iterable = None,
+) -> Iterable:
+    day = day if day else range(1, 31 + 1)
+    year = year if year else range(1900, 2100 + 1)
+    month = month if month else range(1, 12 + 1)
+    dates = (mk_date(y, m, d) for y, m, d in product(year, month, day))
+    dates = filter(None, dates)
+    return dates
+
+
+def enum_birth_by_age(ages: Iterable, base: date = date.today()):
+    ms = MorseSegment([
+        Segment(
+            base.replace(base.year - age, 1, 1),
+            base.replace(base.year - age, 12, 31),
+        )
+        for age in ages
+    ])
+    return iter(ms)
+
+
+def enum_seq(male: bool = False, female: bool = False) -> Iterable:
+    match male, female:
+        case True, False:
+            seqs = range(1, 995 + 1, 2)
+        case False, True:
+            seqs = range(2, 995 + 1, 2)
+        case _:
+            seqs = range(1, 995 + 1)  # 996~999 是百岁老人专用号码
+
+    return map(lambda i: f'{i:03d}', seqs)
+
+
+def patch_checksum(*digits) -> str:
+    number = ''.join(digits[0])
+    if len(number) < 17:
+        raise ValueError()
+
+    return number[:17] + {
+        0: '1', 1: '0', 2: 'X', 3: '9', 4: '8', 5: '7',
+        6: '6', 7: '5', 8: '4', 9: '3', 10: '2',
+    }[sum(map(lambda d, r: int(d) * r, number, RIGHTS)) % 11]
 
 
 class Age(ParamType):
@@ -44,18 +116,18 @@ class Checksum(ParamType):
         self.fail(msg, param, ctx)
 
 
-@click.command(help='穷举所有可能的身份证号码。')
-@click.option('-p', '--province', multiple=True, help='省级代码，可输入多个。')
-@click.option('-c', '--city', multiple=True, help='市级代码，可输入多个。')
-@click.option('-t', '--county', multiple=True, help='县级代码，可输入多个。')
-@click.option('-y', '--year', multiple=True, type=int, help='出生年份。可以输入多个。')
-@click.option('-m', '--month', multiple=True, type=int, help='出生月份。可以输入多个。')
-@click.option('-d', '--day', multiple=True, type=int, help='出生日期（指公历月份的哪一天）。可以输入多个。')
-@click.option('-a', '--age', type=Age(), help='年龄（18）、范围（[0,18]）、集合（{12,18,20}）。')
-@click.option('-M', '--male', is_flag=True, help='男性。')
-@click.option('-F', '--female', is_flag=True, help='女性。男女同时选择等效于同时不选择。')
-@click.option('-s', '--checksum', multiple=True, help='校验码。身份证最后一位。可输入多个。')
-@click.option('-f', '--force', is_flag=True, help='不提示数量，直接输出。')
+@command(help='穷举所有可能的身份证号码。')
+@option('-p', '--province', multiple=True, help='省级代码，可输入多个。')
+@option('-c', '--city', multiple=True, help='市级代码，可输入多个。')
+@option('-t', '--county', multiple=True, help='县级代码，可输入多个。')
+@option('-y', '--year', multiple=True, type=int, help='出生年份。可以输入多个。')
+@option('-m', '--month', multiple=True, type=int, help='出生月份。可以输入多个。')
+@option('-d', '--day', multiple=True, type=int, help='出生日期（指公历月份的哪一天）。可以输入多个。')
+@option('-a', '--age', type=Age(), help='年龄（18）、范围（[0,18]）、集合（{12,18,20}）。')
+@option('-M', '--male', is_flag=True, help='男性。')
+@option('-F', '--female', is_flag=True, help='女性。男女同时选择等效于同时不选择。')
+@option('-s', '--checksum', multiple=True, help='校验码。身份证最后一位。可输入多个。')
+@option('-f', '--force', is_flag=True, help='不提示数量，直接输出。')
 def enumidc(
         province, city, county,
         year, month, day, age,
---
+++ core/structs.py
@@ -0,0 +1,50 @@
+from datetime import date, timedelta
+from itertools import chain
+from typing import NamedTuple
+
+
+class Segment(NamedTuple):
+    start: date
+    stop: date
+    unit = timedelta(days=1)
+
+    def __iter__(self):
+        sign = self.start
+        times = 0
+        while sign < self.stop:
+            sign = self.start + self.unit * times
+            times += 1
+            yield sign.strftime('%Y%m%d')
+
+    def __or__(self, other):
+        assert isinstance(other, self.__class__)
+        if not self.start <= other.start:
+            return other.__or__(self)
+        if self.stop + self.unit < other.start:  # 不相交，无并集
+            return None
+        if self.stop < other.stop:
+            return Segment(self.start, other.stop)
+        return self
+
+
+class MorseSegment:
+    def __init__(self, segments: list[Segment]):
+        segments = sorted(segments, key=lambda s: (s[0], s[1]))
+        results = []
+        anchor = segments.pop(0)
+        for segment in segments:
+            if (union := anchor | segment) is None:
+                results.append(anchor)
+                anchor = segment
+            elif union is anchor:
+                pass
+            else:
+                anchor = union
+        else:
+            results.append(anchor)
+        self._segments = results
+
+    def __iter__(self):
+        print(self._segments)
+        c = chain.from_iterable(self._segments)
+        return c
