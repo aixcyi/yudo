6cfd51508049e70b1396c5253537b22355ac514b
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-02-03 14:41:33 +0800

    fix: 补充 randbit 缺失的选项

--- clis/binary.py
+++ clis/binary.py
@@ -5,7 +5,7 @@ from typing import Final, Any
 import click
 from click.shell_completion import CompletionItem
 
-from core.binary import HexBytes, DecBytes
+from core.binary import HexBytes, DecBytes, BaseBytes
 
 
 class BitLength(click.ParamType):
@@ -42,21 +42,27 @@ class BitLength(click.ParamType):
 @click.command('randbit')
 @click.argument('bits', type=BitLength())
 @click.option('-q', '--qty', type=int, default=1, help='生成多少串字节串（每行一串）。')
-@click.option('-d', '--dec', 'decimal', is_flag=True, help='以十进制字节格式输出。默认输出HEX。')
-@click.option('-i', '--int', 'integer', is_flag=True, help='以整数形式输出。默认输出HEX。')
+@click.option('-x', '--hex', 'hexadecimal', is_flag=True, help='以十六进制数组（HEX）格式输出。')
+@click.option('-d', '--dec', 'decimal', is_flag=True, help='以十进制数组格式输出。默认输出HEX。')
+@click.option('-i', '--int', 'integer', is_flag=True, help='以单个整数形式输出。默认输出HEX。')
+@click.option('--b64', '--base64', is_flag=True, help='base64编码后输出。默认输出HEX。')
+@click.option('--b85', '--base85', is_flag=True, help='base85编码后输出。默认输出HEX。')
+@click.option('--b32', '--base32', is_flag=True, help='base32编码后输出。默认输出HEX。')
+@click.option('--group', default=1, type=int, help='多少字节一组（默认1）。')
 @click.option('--seperator', default='', help='每组之间的间隔符。')
 @click.option('--prefix', default='', help='每组字节的前缀。')
 @click.option('--suffix', default='', help='每组字节的后缀。')
 @click.option('--head', default='', help='开头的前缀。')
 @click.option('--tail', default='', help='结尾的尾缀。')
-@click.option('--group', default=1, type=int, help='多少字节一组（默认1）。')
 def generate_bits(
-        bits, qty, decimal, integer, group,
-        seperator, prefix, suffix, head, tail
+        bits, qty, hexadecimal, decimal, integer, b64, b85, b32,
+        group, seperator, prefix, suffix, head, tail
 ):
     """随机生成 BITS 比特的字节串。"""
     if integer:
         op = None
+    elif b64 or b85 or b32:
+        op = BaseBytes(64 if b64 else 85 if b85 else 32)
     elif decimal:
         op = DecBytes(seperator, prefix, suffix, head, tail)
     else:
