80212e6050328f6912d26450a0163c6dc6e68cea
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-02-05 17:43:30 +0800

    fix: 改进启动脚本以兼容conda

--- yu.bat
+++ yu.bat
@@ -1,3 +1,18 @@
-@echo off
-set py=""
-cmd /c "%py%python main.py %*"
+@ECHO OFF
+
+@REM 直接使用特定的Python解释器来运行，适用于安装了多版本Python或virtualenv时。
+@SET PYTHON_PATH=""
+
+@REM 使用conda环境运行时，conda环境的名称。
+@SET CONDA_ENV="yutils"
+
+@REM 如果 yu start 无法启动conda环境，请尝试修改以下配置。
+@SET CONDA_PATH="C:\ProgramData\Miniconda3\condabin"
+
+TITLE yudo
+CD %~dp0
+IF "%1"=="start" (
+    %CONDA_PATH%\conda activate %CONDA_ENV%
+) ELSE (
+    CMD /C "%PYTHON_PATH%python %~dp0\main.py %*"
+)
