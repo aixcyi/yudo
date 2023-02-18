79302d057b41502400aaedbf2b5e17522cb2288e
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-02-18 12:50:14 +0800

    fix: 补充 exec 命令到click中

--- clis/__init__.py
+++ clis/__init__.py
@@ -5,7 +5,7 @@ from clis.datetime import enum_date, enum_datetime
 from clis.frp import manage_frpc
 from clis.idcard import enum_prcid
 from clis.sequence import product_columns
-from clis.util import split_url, encode_uri, decode_uri, get_length
+from clis.util import split_url, encode_uri, decode_uri, get_length, run_command
 
 interface_list = [
     get_adcode,
@@ -21,4 +21,5 @@ interface_list = [
     get_length,
     configurate,
     manage_frpc,
+    run_command,
 ]
