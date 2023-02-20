b4d26bc19b7c3c1a381a9fcdebaeaa55cd16e256
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-02-20 12:05:52 +0800

    refactor: 优化frpc指令

--- clis/frp.py
+++ clis/frp.py
@@ -84,13 +84,12 @@ def frpc_list_configs():
 
 
 @manage_frpc.command('set', short_help='查看或修改frpc的配置')
-@click.argument('key', metavar='SECTION[.KEY', default='', required=False)
-@click.argument('value', metavar='NEW_VALUE', required=False)
+@click.argument('pattern', metavar='[SECTION[.KEY[=VALUE]]]', required=False)
 @click.option('-c', '--config', 'filename', metavar='NAME',
               help='要修改哪个配置文件，提供简短名称即可。比如 frpc_full.ini 的简短名称就是 full')
 @click.help_option('-h', '--help', help='列出这份帮助信息。')
 def frpc_set_configs(
-        key: str, value: str | None, filename: str | None,
+        pattern: str | None, filename: str | None,
 ):
     """
     设置一个配置项，或者查看配置项。
@@ -99,15 +98,13 @@ def frpc_set_configs(
       - 打印文件："yu frpc set"
       - 打印某节："yu frpc set common"
       - 打印配置："yu frpc set common.server_port"
-      - 修改配置："yu frpc set common.server_port 65535"
+      - 修改配置："yu frpc set common.server_port=65535"
 
     默认围绕 frpc.ini 进行操作，假设要对  frpc_full.ini 操作，就需要附加 -c full 选项。
     """
     if not (cfp := get_cfp(filename)):
         return
-    section, _, key = key.rpartition('.')
-    if not section:
-        section, key = key, ''
+    section, key, value = AutoReadConfigPaser.parse_path(pattern)
 
     with AutoReadConfigPaser(cfp, auto_patch=True) as configs:
         # 打印整个配置文件
@@ -115,9 +112,7 @@ def frpc_set_configs(
             configs.print_all(nl=True)
             return
 
-        if section not in configs:
-            click.secho(f'找不到 {section} 。', err=True, fg=PT_WARNING)
-            click.secho('注意：节名称是区分大小写的。', err=True, fg=PT_SPECIAL)
+        if not configs.check_section(section):
             return
 
         # 打印一整个section
@@ -125,9 +120,7 @@ def frpc_set_configs(
             configs.print_section(section)
             return
 
-        if key not in configs[section]:
-            click.secho(f'在 {section} 中找不到 {key} 。', err=True, fg=PT_WARNING)
-            click.secho('注意：名称是区分大小写的。', err=True, fg=PT_SPECIAL)
+        if not configs.check_key(section, key):
             return
 
         # 打印具体配置
@@ -142,13 +135,13 @@ def frpc_set_configs(
 @manage_frpc.command('run', short_help='运行frp客户端')
 @click.option('-c', '--config', 'filename', metavar='NAME',
               help='用某个配置文件来运行。提供简短名称。使用 yu frpc conf -l 查看所有。')
-@click.option('-s', '--set', 'new_configs', metavar='SECTION.KEY VALUE',
-              multiple=True, nargs=2, help='修改并保存配置后再运行。')
+@click.option('-s', '--set', 'new_configs', metavar='SECTION.KEY=VALUE',
+              multiple=True, help='修改并保存配置后再运行。使用多个 -s 来修改多个配置。')
 @click.option('-p', '-print', 'print_it', is_flag=True,
               help='运行前打印文件中的所有配置。')
 def run_frpc(
         filename: str | None,
-        new_configs: tuple[tuple[str, str]] | None,
+        new_configs: tuple[str] | None,
         print_it: bool,
 ):
     if not (cfp := get_cfp(filename)):
@@ -156,9 +149,8 @@ def run_frpc(
 
     if new_configs:
         with AutoReadConfigPaser(cfp, auto_patch=True) as configs:
-            for c in new_configs:
-                snk, value = c
-                section, _, key = snk.rpartition('.')
+            for new_config in new_configs:
+                section, key, value = AutoReadConfigPaser.parse_path(new_config)
                 if not all([section, key, value]):
                     click.secho(f'配置格式无法解析。', err=True, fg=PT_WARNING)
                     return None
