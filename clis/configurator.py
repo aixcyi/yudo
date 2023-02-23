b03122e6aac31994eb1c4a2a0f0e1a0511775ef3
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-02-23 10:26:38 +0800

    fix: 补充对frps配置的支持

--- clis/configurator.py
+++ clis/configurator.py
@@ -68,6 +68,50 @@ def find_frp_config(
     return cfp
 
 
+def configurate_frp(instruction, pattern: str, filename: str, list_all: bool, delete_it: bool):
+    try:
+        path = get_frp_install_path()
+    except (KeyError, FileNotFoundError):
+        warning(
+            '请先使用以下命令设置 frp 的安装目录：',
+            cmd(configurate, 'conf', 'yudo', 'frp.path=YOUR_INSTALL_PATH'),
+        )
+        return
+    except TypeError:
+        warning(
+            '请使用以下命令重新设置 frp 的安装目录：',
+            cmd(configurate, 'conf', 'yudo', 'frp.path=YOUR_INSTALL_PATH'),
+        )
+        return
+
+    if list_all:
+        table = Table('简短名称', '查看方式', '文件名', box=box.SIMPLE_HEAD)
+        for name, info in find_frp_configs(path, instruction.name).items():
+            if name == '':
+                command = cmd(configurate, instruction)
+            else:
+                command = cmd(configurate, instruction, '-c', name)
+            table.add_row(name, command, info[1])
+        else:
+            console = Console()
+            console.print(table)
+    else:
+        try:
+            cfp = find_frp_config(path, instruction.name, filename)
+        except TypeError as e:
+            warning(f'这不是一个文件：{e.args[0]!s}')
+            return
+        except FileNotFoundError as e:
+            cfp = e.args[0]
+            warning(f'找不到配置文件：{cfp!s}')
+            if not ask('创建它？(Y/[n]) '):
+                return
+            cfp.touch()
+
+        parser = AutoReadConfigPaser(cfp)
+        curd(parser, pattern, delete_it)
+
+
 @click.group('conf', short_help='读取或写入配置文件')
 @click.help_option('-h', '--help', help='列出这份帮助信息。')
 def configurate():
@@ -91,44 +135,14 @@ def configurate_yudo(pattern: str, delete_it: bool):
 @click.option('-d', '--delete', 'delete_it', is_flag=True, help='删除一个配置项或整个配置节。')
 @click.help_option('-h', '--help', help='列出这份帮助信息。')
 def configurate_frpc(pattern: str, filename: str, list_all: bool, delete_it: bool):
-    try:
-        path = get_frp_install_path()
-    except (KeyError, FileNotFoundError):
-        warning(
-            '请先使用以下命令设置 frp 的安装目录：',
-            cmd(configurate, 'conf', 'yudo', 'frp.path=YOUR_INSTALL_PATH'),
-        )
-        return
-    except TypeError:
-        warning(
-            '请使用以下命令重新设置 frp 的安装目录：',
-            cmd(configurate, 'conf', 'yudo', 'frp.path=YOUR_INSTALL_PATH'),
-        )
-        return
+    configurate_frp(configurate_frpc, pattern, filename, list_all, delete_it)
 
-    if list_all:
-        table = Table('简短名称', '查看方式', '文件名', box=box.SIMPLE_HEAD)
-        for name, info in find_frp_configs(path, 'frpc').items():
-            if name == '':
-                command = cmd(configurate, configurate_frpc)
-            else:
-                command = cmd(configurate, configurate_frpc, '-c', name)
-            table.add_row(name, command, info[1])
-        else:
-            console = Console()
-            console.print(table)
-    else:
-        try:
-            cfp = find_frp_config(path, 'frpc', filename)
-        except TypeError as e:
-            warning(f'这不是一个文件：{e.args[0]!s}')
-            return
-        except FileNotFoundError as e:
-            cfp = e.args[0]
-            warning(f'找不到配置文件：{cfp!s}')
-            if not ask('创建它？(Y/[n]) '):
-                return
-            cfp.touch()
 
-        parser = AutoReadConfigPaser(cfp)
-        curd(parser, pattern, delete_it)
+@configurate.command('frps', short_help='配置 frp 服务端')
+@click.argument('pattern', metavar='[SECTION[.KEY[=VALUE]]]', default='', required=False)
+@click.option('-l', '--list', 'list_all', is_flag=True, help='列举所有配置文件及相应的短名称。')
+@click.option('-c', '--config', 'filename', default='', help='配置哪个文件。提供简短名称。')
+@click.option('-d', '--delete', 'delete_it', is_flag=True, help='删除一个配置项或整个配置节。')
+@click.help_option('-h', '--help', help='列出这份帮助信息。')
+def configurate_frps(pattern: str, filename: str, list_all: bool, delete_it: bool):
+    configurate_frp(configurate_frps, pattern, filename, list_all, delete_it)
